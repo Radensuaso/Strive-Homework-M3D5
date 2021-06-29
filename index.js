7a20f3c229f285b5e2657083fc9841cb2dbdb7d6
Radensuaso <andrels9283@gmail.com>
2021-06-29 13:54:17 +0100

    Ex 2 almost done

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ window.onload = () => {
 
 const generateTr = function (user) {
   return `<th scope="row">${user.id}</th>
-            <td>${user.name}</td>
-            <td>${user.username}</td>
-            <td>${user.email}</td>
+            <td>
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown button
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">${user.name}</a>
+                        <a class="dropdown-item" href="#">${user.username}</a>
+                        <a class="dropdown-item" href="#">${user.email}</a>
+                    </div>
+                </div>
+            </td>
+            <td>${user.address.city}</td>
+            <td>${user.phone}</td>
+            <td>${user.company.name}</td>
             <td>${user.website}</td>`
 }
 
