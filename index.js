b8b419c14d84b398082986d280d3080511c76df4
Radensuaso <andrels9283@gmail.com>
2021-06-29 15:54:40 +0100

    Search in progress

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const log = console.log
 
 window.onload = () => {
   getUsers()
-  const inputField = document.querySelector("#user-search-value")
-  inputField.addEventListener("keyup", searchUsers)
 }
 
 const generateTr = function (user) {
@@ -12,9 +10,9 @@ const generateTr = function (user) {
             <td>
               <select class="form-select" aria-label="Default select example">
                 <option selected>Select</option>
-                <option class="user-select" value="user-name">${user.name}</option>
-                <option class="user-select" value="user-username">${user.username}</option>
-                <option class="user-select" value="user-email">${user.email}</option>
+                <option class="user-name" value="user-name">${user.name}</option>
+                <option class="user-username" value="user-username">${user.username}</option>
+                <option class="user-email" value="user-email">${user.email}</option>
               </select>
             </td>
             <td>${user.address.city}</td>
@@ -33,12 +31,14 @@ const getUsers = async function () {
   const tBody = document.querySelector("#users-table-container table tbody")
 
   //Create row for each element
-  users.forEach( user => {
+  users.forEach((user) => {
     console.log(user)
     tBody.innerHTML += generateTr(user)
   })
-  
-  searchUsers()
+
+  //add event listener to input text
+  const inputField = document.querySelector("#user-search-value")
+  inputField.addEventListener("keyup", searchUsers)
 }
 
 // const searchUsers = function () {
@@ -48,11 +48,19 @@ const getUsers = async function () {
 
 const searchUsers = function () {
   let searchValue = this.value
-  let users = document.getElementsByClassName("user-select")
-  log(users[0].value)
-  
-  let users2 = document.getElementsByTagName("tr")
-  log(users2)
-  log(users)
-  
+  //let users = document.getElementsByClassName("user-select")
+  //log(users[0].value)
+  let trArray = document.getElementsByTagName("tr")
+  console.log(trArray)
+  const arrayName = []
+  ;[...trArray].forEach((tr) => {
+    const name = tr.querySelector(".user-name")
+    const userName = tr.querySelector(".user-username")
+    const email = tr.querySelector(".user-email")
+    arrayName.push(name)
+
+    console.log(name)
+  })
+
+  //log(users)
 }
